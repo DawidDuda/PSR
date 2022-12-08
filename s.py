5cf90e974a2a04a8a8837a07dbf13d87e759f750
DawidDuda <117982822+DawidDuda@users.noreply.github.com>
2022-12-08 22:37:34 +0100

    Update s.py

--- s.py
+++ s.py
@@ -1,43 +1,58 @@
+import time
+import threading
 import socket
 import os
-import subprocess
-import threading
-from tkinter import *
-from tkinter import ttk
+import shutil
 
-host = '192.168.101.10' #Server ip
-port = 4000
+host='192.168.101.11' #client ip
+port = 4005
+server = ('192.168.101.10', 4000)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host, port))
- 
- 
- 
- 
-def snd_dwn(prog_name):
- 
-    data, addr = s.recvfrom(1024)
-    data = data.decode('utf-8')
-    print("Message from: " + str(addr))
-    print("From connected user: " + data)      
+s.bind((host,port))
 
- 
-    print("Sending...")
-    file = open(prog_name, 'rb')
-    filedata = file.read(99999)
-    s.sendto(bytes(filedata), addr)
- 
- 
- 
-class download(threading.Thread):
-    def __init__(self,prog_name):
-        threading.Thread.__init__(self)
-        self.prog_name = prog_name
+def receive_req():
+    data, addr = s.recvfrom(9999999999)
+    data=data.decode('utf-8')
+    if(data[0:4] == "run:"):
+        print(data[0:3])
+        print(data[4::])
+        execute(data[4::])
+        print("koniec")
         
-    def run(self):
-        snd_dwn(self.prog_name)
-        print("Wys?ano")
-    
+    if(data[0:4] == "kil:"):
+        print("kill")
+        os.system("taskkill /im" + data[4::])
+        
+def download_prog(message):
+    if(message !='q'):
+        try:
+            
+            message = "snd:"+str(message)
+            s.sendto(message.encode('utf-8'), server)
+            
+            data, addr = s.recvfrom(99999)
+            file = open(message[4::], 'wb')
+            file.write(bytes(data))
+            file.close()
+            print("odebrano")
+            
 
-while True:
-    
-    download("test.txt").start()
+        except:
+            print("Brak połączenia")
+            
+##windows
+def execute(prog_name):
+    print("execute")
+    if(os.path.exists(prog_name)):
+        print("if")
+    #       print(subprocess.check_output(prog_name, shell=True, text=True))
+    #print(os.system('cmd /k "tescik.exe"'))
+    else:
+        download_prog(prog_name)
+        time.sleep(2)
+      #  if(os.path.exists(prog_name)):
+     #          print(subprocess.check_output(prog_name, shell=True, text=True))
+
+execute("unt.py")
+
+
