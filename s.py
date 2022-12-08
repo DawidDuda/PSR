f53a72744e125d7792dd82131f16a5ed906e82b4
DawidDuda <117982822+DawidDuda@users.noreply.github.com>
2022-12-08 23:48:52 +0100

    Update s.py

--- s.py
+++ s.py
@@ -1,58 +1,58 @@
-import time
-import threading
 import socket
 import os
-import shutil
+import time
+import subprocess
+import threading
+from tkinter import *
+from tkinter import ttk
 
-host='192.168.101.11' #client ip
-port = 4005
-server = ('192.168.101.10', 4000)
+host = '192.168.101.10' #Server ip
+port = 4000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host,port))
+s.bind((host, port))
 
-def receive_req():
-    data, addr = s.recvfrom(9999999999)
-    data=data.decode('utf-8')
-    if(data[0:4] == "run:"):
-        print(data[0:3])
-        print(data[4::])
-        execute(data[4::])
-        print("koniec")
+client = [('192.168.101.11', 4005),('192.168.101.156', 4005)]
+ 
+def send_request(prog_name, dstclient):
+    
+    message = "run:" + str(prog_name)
+                
+    s.sendto(message.encode('utf-8'), dstclient)
+                    
+    data, addr = s.recvfrom(99999)
+    data = data.decode('utf-8')
+    print("Message from: " + str(addr))
+    print("From connected user: " + data) 
+    if(data[0:3]== "snd"):
+        print("wysylam" +data[4::])
+        dwn_snd(data[4::])
+            
+ 
+def dwn_snd(prog_name, dstclient):    
+         
+
+ 
+    print("Sending...")
+    file = open(prog_name, 'rb')
+    filedata = file.read(99999)
+    s.sendto(bytes(filedata), dstclient)
+ 
+ 
+ 
+class send_exe(threading.Thread):
+    def __init__(self,prog_name, dstclient):
+        threading.Thread.__init__(self)
+        self.prog_name = prog_name
+        self.dstclient = dstclient
         
-    if(data[0:4] == "kil:"):
-        print("kill")
-        os.system("taskkill /im" + data[4::])
-        
-def download_prog(message):
-    if(message !='q'):
-        try:
-            
-            message = "snd:"+str(message)
-            s.sendto(message.encode('utf-8'), server)
-            
-            data, addr = s.recvfrom(99999)
-            file = open(message[4::], 'wb')
-            file.write(bytes(data))
-            file.close()
-            print("odebrano")
-            
+    def run(self):
+        dwn_snd(self.prog_name, self.dstclient)
+        print("Wys?ano")
+ 
 
-        except:
-            print("Brak połączenia")
-            
-##windows
-def execute(prog_name):
-    print("execute")
-    if(os.path.exists(prog_name)):
-        print("if")
-    #       print(subprocess.check_output(prog_name, shell=True, text=True))
-    #print(os.system('cmd /k "tescik.exe"'))
-    else:
-        download_prog(prog_name)
-        time.sleep(2)
-      #  if(os.path.exists(prog_name)):
-     #          print(subprocess.check_output(prog_name, shell=True, text=True))
+    
 
-execute("unt.py")
-
-
+#dwn_snd("unt.py")
+send_request("unt.py", client[1])
+#while True:   
+#    send_exe("test.txt").start()
