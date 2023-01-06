a16c3ac1ca8738048a76bce58561f3f13cc4e6ec
DawidDuda <117982822+DawidDuda@users.noreply.github.com>
2023-01-07 00:41:28 +0100

    Update s.py

--- s.py
+++ s.py
@@ -1,45 +1,56 @@
 import socket
 import os
 import time
+import sys
 import subprocess
 import threading
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
-host = '127.0.0.1' #Server ip
+import xml.etree.ElementTree as ET
+
+host = '192.168.101.10' #Server ip
 port = 4000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host, port))
 
-class data:
-    filename = ""
 
-## zrobic wczytywanie z pliku konfiguracyjnego
-client = [('127.0.0.1', 4005),('192.168.101.156', 4005)]
+                                           #Empty list to collect unreachable hosts
 
-def send_request(prog_name, dstclient):
     
-    message = "run:" + str(prog_name)
-                
-    s.sendto(message.encode('utf-8'), dstclient)
-                    
-    data, addr = s.recvfrom(99999)
-    data = data.decode('utf-8')
-    print("Message from: " + str(addr))
-    print("From connected user: " + data) 
-    if(data[0:3]== "snd"):
-        print("wysylam" +data[4::])
-        snd(data[4::], dstclient)
+            
+def send_request(prog_name, dstclient):
+    for ip in dstclient:
+        ping_test = subprocess.call('ping %s -n 2' % ip)        #Ping host n times
+        if ping_test == 0: 
+            message = "run:" + str(prog_name)
+                        
+            s.sendto(message.encode('utf-8'), dstclient)
+            data, addr = s.recvfrom(99999)
+            data = data.decode('utf-8')
+            print("Message from: " + str(addr))
+            print("From connected user: " + data) 
+            
+            Main.status_lbl.config(text = data)
+            
+            print("Sending " +data[4::])
+            Main.status_lbl.config(text = "Sending")
+            file = open(prog_name, 'rb')
+            filedata = file.read(99999)
+            s.sendto(bytes(filedata), dstclient)
+            
+            print("Sended")
+            Main.status_lbl.config(text = "Sended")
+            
+            data, addr = s.recvfrom(99999)
+            data = data.decode('utf-8')
+            print("Message from: " + str(addr))
+            print("From connected user: " + data) 
+            Main.status_lbl.config(text = data)
             
  
-def snd(prog_name, dstclient):    
 
-    print("Sending...")
-    file = open(prog_name, 'rb')
-    filedata = file.read(99999)
-    s.sendto(bytes(filedata), dstclient)
- 
- 
+        
  
 class send_exe(threading.Thread):
     def __init__(self,prog_name, dstclient):
@@ -49,96 +60,84 @@ class send_exe(threading.Thread):
         
     def run(self):
         send_request(self.prog_name, self.dstclient)
-        print("Wys?ano")
+        
  
 
+        
     
 
-#snd("unt.py")
-#send_request("unt.py", client[1])
-#while True:   
-#send_exe("unt.py", client[1]).start()
 ##################### GUI ########################
 def add_client(ip):
-    client.append((ip, 4005))
+    Main.client.append((ip, 4005))
     
 def print_c():
-    for i in range(len(client)):
-        print(client[i])
+    for i in range(len(Main.client)):
+        print(Main.client[i])
+        print(i)
+        Main.client_lbl[i]
+        
 
 def run_exe():
     for i in range(get_number_cli()):
-        send_exe(data.filename, client[i]).start()
-
-def get_filename():
-    point = 0
-    for i in range(len(data.filename)):
-        if(data.filename[i]=='/'):
-            point = i+1
-            
-    data.filename = data.filename[point::]
+        send_exe(Main.filename, Main.client[i]).start()
+        
+    
+ 
    
 
 def get_number_cli():
-    return len(client)     
+    return len(Main.client)     
     
+def readXML():
+    tree = ET.parse('config.xml')
     
-def MainWindow():
+    root = tree.getroot()
+    
+    file=root.find("file")
+    Main.filename = (file.text)
+    Main.filename_lbl.config(text = (file.text))
+    
+    #command = root.find("command")
+    #Main.command = (command.text)
+    
+    args = root.find("args")
+    Main.args = (args.text)
+    
+    for client in root.iter('client'):
+        Main.client.append((client.text, 4005))
+        Main.client_lbl.append(tk.Label(master=Main.window, text=client.text))
+    
+
+
+class Main:
     window = tk.Tk()
-    window.title("PSR")
-    window.geometry("600x400")
-    frame1 = tk.Frame(master=window,relief=tk.RAISED, width=200, height=100)
-    frame1.pack( side=tk.RIGHT)
-    frame2 = tk.Frame(master=window,relief=tk.RAISED,  height=30)
-    frame2.pack( side=tk.BOTTOM)
-    
-    def add_btn():
-        inp = inputtxt.get(1.0, "end-1c")
-        add_client(inp)
-        print_cli()
-        #lbl.config(text = "Provided Input: "+inp)
-        
-    def choose_file():
-        data.filename = fd.askopenfilename()
-        
-    def print_cli():
-        for i in range(len(client)):
-            label = tk.Label(master=frame1, text=client[i][0])
-            label.pack()
-            
-            
-   
-    
-    
-    label = tk.Label(master=frame1, text="test")
-    label.pack()
-    
-    print_cli()
-    get_filename()
-    filename_lbl = tk.Label(master=frame2, text=data.filename)
-    filename_lbl.pack()
-    
-    
-    inputtxt = tk.Text(window,
-                   height = 1,
-                   width = 20)
-    inputtxt.pack()
-    
-    
+    filename = ""
+    #command = ""
+    args = ""
+    client = []
+    client_lbl = []
+    filename_lbl = tk.Label(master=window, text=filename)
+    status_lbl = tk.Label(master=window, text="Status programu")
     
     runButton = tk.Button(window,
                         text = "uruchom zdalnie", 
                         command = run_exe)
-    runButton.pack()
     
-    fileChoserButton = tk.Button(window,
-                        text = "Wybierz plik exe", 
-                        command = choose_file)
-    fileChoserButton.pack()
-    #
+
     
-    print("file"+data.filename)
+def MainWindow():
     
-    window.mainloop()
+    Main.window.title("PSR")
+    Main.window.geometry("600x400")
+    
+    readXML()
+    Main.filename_lbl.pack()
+    Main.runButton.pack()
+    Main.status_lbl.pack()
+    
+    print_c()
+    
+    Main.window.mainloop()
+    s.close()
 
 MainWindow()
